60d42d364639a490dab9f602613265499b3eda6a
preetmodi <preet.modi00@gmail.com>
2020-02-20 10:32:33 +0530

    commit
    BADHU THAY CHE!!!!!!!!!!!

--- kiran.py
+++ kiran.py
@@ -2,31 +2,60 @@ import cv2
 import numpy as np
 import imutils
 import pytesseract
-def detect_rectangles(image):
+    def detect_rectangles(image):
+        dst = image.copy()
+        img=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        _, threshold = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
+        contours,_=cv2.findContours(threshold,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+        coordinate=[]
+        co=0
+        text = ""
+        ep =1
+    #    bbox = cv2.boundingRect(contours[51])
+    #    x,y,w,h = bbox
+    #    text = pytesseract.image_to_string(img[y-ep:y+h+ep,x-ep:x+w+ep], config="-c tessedit"
+    #                                       "_char_whitelist=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    #                                       " --psm 10"
+    #                                       " -l osd" 
+    #                                       " ")
+    #    print(text)
+    #    cv2.imshow("crop",img[y-ep:y+h+ep,x-ep:x+w+ep])
+    #    cv2.waitKey(0)
+    #    cv2.destroyAllWindows()
+        for cnt in contours:
+        bbox = cv2.boundingRect(cnt)
+        x,y,w,h = bbox
+        if w<2 or h<2 or w*h<10 or w>1000:
+            continue
+        text = pytesseract.image_to_string(img[y:y+h,x:x+w],config="-c tessedit"
+                                       "_char_whitelist=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+                                       " --psm 10"
+                                       " -l osd" 
+                                       " ")
+        print(text,w,h)
+        
+        cv2.rectangle(dst, (x,y),(x+w,y+h),(0,0,255),2)
+    cv2.imwrite("boundingbox.jpg",dst)
+    print(text)
     
-    img=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    _, threshold = cv2.threshold(img, 240, 255, cv2.THRESH_BINARY)
-    contours,_=cv2.findContours(threshold,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
-    coordinate=[]
-    co=0
-    for cnt in contours:
-
-        approx = cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt, True), True)
-        if len(approx) == 4 and cv2.contourArea(cnt)>100:
-            '''if 2000+co > cv2.contourArea(cnt) > co:         
-                continue
-                co=cv2.contourArea(cnt)'''# try changing the value in place of 2000 to get outer rectangles
-            coordinate.append((approx[0][0],approx[2][0]))
-            
-    print(coordinate[0][0])# top-left coordinate of 1 rectangle
-    print(coordinate[0][1])# bottom-right coordinate of 1 rectangle
-    print(coordinate[1][0])# top-left coordinate of 2 rectangle
-    print(coordinate[1][1])# bottom-right coordinate of 2 rectangle
-    #and so on
-    for i in range(len(coordinate)):
-        cv2.circle(img,tuple(coordinate[i][0]),5,(0,255,0),5)
-        cv2.circle(img,tuple(coordinate[i][1]),5,(0,255,0),5)
-    return img,coordinate
+    
+#
+#        approx = cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt, True), True)
+#        if len(approx) == 4 and cv2.contourArea(cnt)>50:
+#            '''if 2000+co > cv2.contourArea(cnt) > co:         
+#                continue
+#                co=cv2.contourArea(cnt)'''# try changing the value in place of 2000 to get outer rectangles
+#            coordinate.append((approx[0][0],approx[2][0]))
+#            
+#    print(coordinate[0][0])# top-left coordinate of 1 rectangle
+#    print(coordinate[0][1])# bottom-right coordinate of 1 rectangle
+#    print(coordinate[1][0])# top-left coordinate of 2 rectangle
+#    print(coordinate[1][1])# bottom-right coordinate of 2 rectangle
+#    #and so onn
+#    for i in range(len(coordinate)):
+#        cv2.circle(img,tuple(coordinate[i][0]),5,(0,255,0),5)
+#        cv2.circle(img,tuple(coordinate[i][1]),5,(0,255,0),5)
+#    return img,coordinate
 def detect_circles(image):
     img=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1,img.shape[0]/64,
@@ -43,9 +72,10 @@ def detect_circles(image):
         cv2.rectangle(img,tuple(coordinate[i][0]),tuple(coordinate[i][1]),(0,255,0),2)
     return img,coordinate
 
-image=cv2.imread("test20.jpg")
+image=cv2.imread("test22.jpg")
 #image=imutils.resize(image, width = 1000)
-rectangle_image,rec_coordinate=detect_rectangles(image)
+#rectangle_image,rec_coordinate=detect_rectangles(image)
+detect_rectangles(image)
 #image=cv2.imread("Test1.jpg")  
 #image=imutils.resize(image, width = 1000)
 #for row in rec_coordinate:
--- temp.py
+++ temp.py
@@ -7,55 +7,51 @@ import pandas as pd
 
 
 
-def contour(img):
-    #ratio = img.shape[0]/float(img.shape[1])
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blurr = cv2.GaussianBlur(gray, (5,5),0)
-    thres = 100
-    ret,gray = cv2.threshold(blurr,thres,255,cv2.THRESH_BINARY)
-    #gray = cv2.Canny(blurr, 50,150, apertureSize = 3)
-
-    print("Thresahold" + str(thres))
-    cv2.imshow("Thres", gray)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    contours = cv2.findContours(gray.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = imutils.grab_contours(contours)
+def contour(image):
+    img=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    _, threshold = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
+    contours,_=cv2.findContours(threshold,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
     return contours
 def findCircle(cnts,img1):
     if len(cnts) == 0:
         print("No contour found!!")
     else:
+        print(len(cnts))
         print("Starting circle search")
         img = img1.copy()
         circles = []
 		# find the largest contour in the mask, then use
 		# it to compute the minimum enclosing circle andSub1v4V
 		# centroid
-        for i in range(len(cnts)):
+        for c in cnts:
             print("Inside Loop")
-            c = max(cnts, key=cv2.contourArea)
             ((x, y), radius) = cv2.minEnclosingCircle(c)
             M = cv2.moments(c)
             center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
             c_area = cv2.contourArea(c)
             c_area = c_area/((3.14)*radius*radius)
-            if radius>3:
-                if radius<25:
+            if radius>10:
+                print("radius > 3")
+                if radius<50:
+                    print("Radius < 50")
                     if c_area>0.65:
+                        print("C_area > 0.65")
                         circles.append([int(x), int(y), radius, radius, "OMR", np.nan, 0])
                         cv2.circle(img, (int(x), int(y)), int(radius),(0,0,255), 1)
 #                        cv2.circle(imgout,(int(x), int(y)), int(radius),(0,0,255), 2)
 #                        cv2.circle(imgout, center, 5, (0, 0, 255), -1)
                         cv2.circle(img, center, 5, (0, 0, 255), -1)
 #                        cv2.ellipse(img3,ellipse,(255,0,0),2)
-                        cv2.imshow("contour", img)
-                        cv2.waitKey(0)
-                        cv2.destroyAllWindows()
+                        
             else:
                 print("Small Contours Found")
-            cnts.remove(c)
+        cv2.imshow("contour", img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        cv2.imwrite("circle.png",img)
+        print(len(circles))
         return circles
+    
 """
             
         cv2.drawContours(mask, [c], -1, 0, -1)
@@ -91,6 +87,8 @@ def boxDetect(c):
     else:
         return 0,0,0,0 
 """
+
+
 
 def linesp(img):
 #    img1 = img.copy()
@@ -134,7 +132,7 @@ for number in range(22,23):
     height = 50
     diff = 30
     img = imutils.resize(img, width=width)
-    
+    img3 = img.copy()
 #    img = img[150:250,:]
     img1 = img.copy()
     img2 = img.copy()
@@ -142,10 +140,14 @@ for number in range(22,23):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 #"""Label Detection and Processing"""
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, threshold = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+    gray = cv2.erode(threshold, None)
+    gray = cv2.dilate(gray, None)
     
-    text = pytesseract.image_to_string(img)
+#    text = pytesseract.image_to_string(img)
 #    print(text)
-    text = pytesseract.image_to_boxes(img)
+    text = pytesseract.image_to_boxes(gray)
     data = text.split('\n')
     text = []
     label_box = []
@@ -186,8 +188,45 @@ for number in range(22,23):
     label_box.append([X1,Y1,X2,Y2,"Label",value,"0"])
 #    print(data)
 #    print(label_box)
-
-
+    
+    coordinates = []
+    rectangle_image,rec_coordinate=detect_rectangles(img)
+    for x,y in rec_coordinate:
+        coordinates.append([x[0],x[1],y[0],y[1]])
+    df_box = pd.DataFrame(coordinates, columns = ['X1','Y1','X2','Y2'])
+    
+    df_box = df_box.sort_values(by= ['Y1','X1']).reset_index(drop=True)
+    print("DataFrame of Rectangle",df_box)
+    x1,y1,x2,y2 = 0,0,0,0
+    for row in df_box.itertuples():
+        index_v = row[0]
+        if row[1]<10 and row[2]<10:
+            df_box = df_box.drop(row[0])        
+        try:
+            while(index_v in df_box.index and abs(y1-df_box.loc[index_v][1])<10):
+                if index_v in df_box.index:
+    #                    print(row[0]-1, y1,index_v, df.loc[index_v][1])
+                    if abs(x1-df_box.loc[index_v][0])<diff and abs(y1-df_box.loc[index_v][1])<diff and abs(x2-df_box.loc[index_v][2])<diff and abs(y2-df_box.loc[index_v][3])<diff:
+                        df_box = df_box.drop(index_v)
+                    else:
+                            pass
+                    index_v+=1
+        except Exception as e:
+            print(e)
+        try:
+            index_curr = row[0]
+            x1 = df_box.loc[row[0]][0]
+            y1 = df_box.loc[row[0]][1]
+            x2 = df_box.loc[row[0]][2]
+            y2 = df_box.loc[row[0]][3]
+        except:
+            pass
+    print("DataFrame of Rectangle",df_box)
+    df_box = df_box.sort_values(by= ['Y1','X1']).reset_index(drop=True)
+    
+  
+            
+    
 #"""Line detection and processing"""    
     line = linesp(img)
     horiz_lines = []
@@ -204,7 +243,7 @@ for number in range(22,23):
         writer = csv.writer(file)
         writer.writerows(df)
 #    df.to_csv("Data"+str(number)+".csv")
-    print(df.to_csv(filename))
+#    print(df.to_csv(filename))
     x1,y1,x2,y2 = 0,0,0,0
     field_box = []
     index_curr = 0
@@ -215,7 +254,7 @@ for number in range(22,23):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     cv2.imwrite("lines_detected.jpg", img2)
-    print(df)
+#    print(df)
     for row in df.itertuples():
         index_v = row[0]
         if row[0] == 35:
@@ -223,14 +262,14 @@ for number in range(22,23):
         try:
             while(index_v in df.index and abs(y1-df.loc[index_v][1])<10):
                 if index_v in df.index:
-                    print(row[0]-1, y1,index_v, df.loc[index_v][1])
+#                    print(row[0]-1, y1,index_v, df.loc[index_v][1])
                     if abs(x1-df.loc[index_v][0])<diff and abs(y1-df.loc[index_v][1])<diff and abs(x2-df.loc[index_v][2])<diff and abs(y2-df.loc[index_v][3])<diff:
                         df = df.drop(index_v)
-                        print(df)
-                        print("one")
+#                        print(df)
+#                        print("one")
                     elif abs(y1-df.loc[index_v][1])<diff and (x1<=df.loc[index_v][0] and x2>=df.loc[index_v][2]):
                         df = df.drop(index_v)
-                        print("Two")
+#                        print("Two")
                     elif abs(y1-df.loc[index_v][1])<diff and (x1<=df.loc[index_v][0] and x2>=df.loc[index_v][0]):
                         field_box.pop()
                         if x2<df.iloc[index_v][2]:
@@ -240,7 +279,7 @@ for number in range(22,23):
                             df = df.drop(index_v)
                         else:
                             df = df.drop(index_v)
-                        print("Three")
+#                        print("Three")
                     elif abs(y1-df.loc[index_v][1])<diff and (x1>=df.loc[index_v][0] and x1<=df.loc[index_v][2]):
                         field_box.pop()
                         if x2>df.loc[index_v][2]:
@@ -251,21 +290,21 @@ for number in range(22,23):
                         else:
                             field_box.append([df.loc[index_v][0], y1-height, df.loc[index_v][2], y1, "Field", np.nan, 0])
                             df = df.drop(index_curr)
-                        print("Four")
+#                        print("Four")
                     elif abs(y1-df.loc[index_v][1])<diff and abs(x2-df.loc[index_v][0])<diff:
                         field_box.pop()
                         field_box.append([x1, y1-height, df.loc[index_v][2], y1, "Field", np.nan, 0])
                         x2 = df.loc[index_v][2]
                         df.loc[index_curr] = [x1, y1, df.loc[index_v][2], y1]
                         df = df.drop(index_v)
-                        print("Five")
+#                        print("Five")
                     elif abs(y1-df.loc[index_v][1])<diff and abs(x1-df.loc[index_v][2])<diff:
                         field_box.pop()
                         field_box.append([df.loc[index_v][0], y1-height, x2, y1, "Field", np.nan, 0])
                         df.loc[index_curr]= [df.loc[index_v][0], y1, x2, y1]
                         x1 = df.loc[index_v][0]
                         df = df.drop(index_v)
-                        print(df)
+#                        print(df)
                     else:
                         pass
                 index_v+=1
@@ -283,80 +322,96 @@ for number in range(22,23):
             
         
     df = df.sort_values(by= ['Y1','X1']).reset_index(drop=True)
-    print("Looop Restart")
-    for row in df.itertuples():
-        index_v = row[0]
-        if row[0] == 35:
-            print("Hello")
-        try:
-            while(index_v in df.index and abs(y1-df.loc[index_v][1])<10):
-                if index_v in df.index:
-                    print(row[0]-1, y1,index_v, df.loc[index_v][1])
-                    if abs(x1-df.loc[index_v][0])<diff and abs(y1-df.loc[index_v][1])<diff and abs(x2-df.loc[index_v][2])<diff and abs(y2-df.loc[index_v][3])<diff:
-                        df = df.drop(index_v)
-                        print(df)
-                        print("one")
-                    elif abs(y1-df.loc[index_v][1])<diff and (x1<=df.loc[index_v][0] and x2>=df.loc[index_v][2]):
-                        df = df.drop(index_v)
-                        print("Two")
-                    elif abs(y1-df.loc[index_v][1])<diff and (x1<=df.loc[index_v][0] and x2>=df.loc[index_v][0]):
-                        field_box.pop()
-                        if x2<df.iloc[index_v][2]:
-                            field_box.append([x1, y1-height, df.loc[index_v][2], y1, "Field", np.nan, 0])
-                            df.loc[index_curr] = [x1, y1, df.loc[index_v][2], y1]
-                            x2 = df.loc[index_v][2]
-                            df = df.drop(index_v)
-                        else:
-                            df = df.drop(index_v)
-                        print("Three")
-                    elif abs(y1-df.loc[index_v][1])<diff and (x1>=df.loc[index_v][0] and x1<=df.loc[index_v][2]):
-                        field_box.pop()
-                        if x2>df.loc[index_v][2]:
-                            field_box.append([df.loc[index_v][0], y1-height, x2, y1, "Field", np.nan, 0])
-                            df.loc[index_curr] = [df.loc[index_v][0], y1, x2, y1]
-                            x1 = df.loc[index_v][0]
-                            df = df.drop(index_v)
-                        else:
-                            field_box.append([df.loc[index_v][0], y1-height, df.loc[index_v][2], y1, "Field", np.nan, 0])
-                            df = df.drop(index_curr)
-                        print("Four")
-                    elif abs(y1-df.loc[index_v][1])<diff and abs(x2-df.loc[index_v][0])<diff:
-                        field_box.pop()
-                        field_box.append([x1, y1-height, df.loc[index_v][2], y1, "Field", np.nan, 0])
-                        df.loc[index_curr] = [x1, y1, df.loc[index_v][2], y1]
-                        x2 = df.loc[index_v][2]
-                        df = df.drop(index_v)
-                        print("Five")
-                    elif abs(y1-df.loc[index_v][1])<diff and abs(x1-df.loc[index_v][2])<diff:
-                        field_box.pop()
-                        field_box.append([df.loc[index_v][0], y1-height, x2, y1, "Field", np.nan, 0])
-                        df.loc[index_curr]= [df.loc[index_v][0], y1, x2, y1]
-                        x1 = df.loc[index_v][0]
-                        df = df.drop(index_v)
-                        print(df)
-                    else:
-                        pass
-                index_v+=1
-        except Exception as e:
-            print(e)
-        try:
-            index_curr = row[0]
-            x1 = df.loc[row[0]][0]
-            y1 = df.loc[row[0]][1]
-            x2 = df.loc[row[0]][2]
-            y2 = df.loc[row[0]][3]
-            field_box.append([x1,y1-height,x2,y2,"Field",np.nan,0])
-        except:
-            pass
     print(df)
+    print("Looop Restart")
+#    for row in df.itertuples():
+#        index_v = row[0]
+#        if row[0] == 35:
+#            print("Hello")
+#        try:
+#            while(index_v in df.index and abs(y1-df.loc[index_v][1])<10):
+#                if index_v in df.index:
+#                    print(row[0]-1, y1,index_v, df.loc[index_v][1])
+#                    if abs(x1-df.loc[index_v][0])<diff and abs(y1-df.loc[index_v][1])<diff and abs(x2-df.loc[index_v][2])<diff and abs(y2-df.loc[index_v][3])<diff:
+#                        df = df.drop(index_v)
+##                        print(df)
+#                        print("one")
+#                    elif abs(y1-df.loc[index_v][1])<diff and (x1<=df.loc[index_v][0] and x2>=df.loc[index_v][2]):
+#                        df = df.drop(index_v)
+#                        print("Two")
+#                    elif abs(y1-df.loc[index_v][1])<diff and (x1<=df.loc[index_v][0] and x2>=df.loc[index_v][0]):
+#                        field_box.pop()
+#                        if x2<df.iloc[index_v][2]:
+#                            field_box.append([x1, y1-height, df.loc[index_v][2], y1, "Field", np.nan, 0])
+#                            df.loc[index_curr] = [x1, y1, df.loc[index_v][2], y1]
+#                            x2 = df.loc[index_v][2]
+#                            df = df.drop(index_v)
+#                        else:
+#                            df = df.drop(index_v)
+#                        print("Three")
+#                    elif abs(y1-df.loc[index_v][1])<diff and (x1>=df.loc[index_v][0] and x1<=df.loc[index_v][2]):
+#                        field_box.pop()
+#                        if x2>df.loc[index_v][2]:
+#                            field_box.append([df.loc[index_v][0], y1-height, x2, y1, "Field", np.nan, 0])
+#                            df.loc[index_curr] = [df.loc[index_v][0], y1, x2, y1]
+#                            x1 = df.loc[index_v][0]
+#                            df = df.drop(index_v)
+#                        else:
+#                            field_box.append([df.loc[index_v][0], y1-height, df.loc[index_v][2], y1, "Field", np.nan, 0])
+#                            df = df.drop(index_curr)
+#                        print("Four")
+#                    elif abs(y1-df.loc[index_v][1])<diff and abs(x2-df.loc[index_v][0])<diff:
+#                        field_box.pop()
+#                        field_box.append([x1, y1-height, df.loc[index_v][2], y1, "Field", np.nan, 0])
+#                        df.loc[index_curr] = [x1, y1, df.loc[index_v][2], y1]
+#                        x2 = df.loc[index_v][2]
+#                        df = df.drop(index_v)
+#                        print("Five")
+#                    elif abs(y1-df.loc[index_v][1])<diff and abs(x1-df.loc[index_v][2])<diff:
+#                        field_box.pop()
+#                        field_box.append([df.loc[index_v][0], y1-height, x2, y1, "Field", np.nan, 0])
+#                        df.loc[index_curr]= [df.loc[index_v][0], y1, x2, y1]
+#                        x1 = df.loc[index_v][0]
+#                        df = df.drop(index_v)
+##                        print(df)
+#                    else:
+#                        pass
+#                index_v+=1
+#        except Exception as e:
+#            print(e)
+#        try:
+#            index_curr = row[0]
+#            x1 = df.loc[row[0]][0]
+#            y1 = df.loc[row[0]][1]
+#            x2 = df.loc[row[0]][2]
+#            y2 = df.loc[row[0]][3]
+#            field_box.append([x1,y1-height,x2,y2,"Field",np.nan,0])
+#        except:
+#            pass
     
+    start = df.iloc[0]
+    end = df.loc[df.last_valid_index()]
+    count = 0
+    for row in df_box.itertuples():
+        for rows in df.itertuples():
+            if row[0] in df_box.index and rows[0] in df.index:
+                if abs(row[1]-rows[1])<diff and abs(row[2]-rows[2])<diff:
+                    df = df.drop(rows[0])
+                    count+=1
+                elif abs(row[3]-rows[3])<diff and abs(row[4]-rows[4])<diff:
+                    df = df.drop(rows[0])
+                    count+=1
+                else:
+                    pass
+    print(count)
+    print(df)
     cv2.imwrite("img"+str(number)+".png",img1)
     with open('data'+str(number) + '.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["X1", "Y1", "X2", "Y2", "Type", "Value", "Group"])
         writer.writerows(label_box)
         writer.writerows(field_box)
-        
+    
     cnt = contour(img1)
     circles = findCircle(cnt, img1)
     cv2.imshow("p", img1)
@@ -365,8 +420,6 @@ for number in range(22,23):
 #img = cv2.imread("Test1.jpg")
 #img = imutils.resize(img, width = width)
 #img1 = img.copy()
-print(img.shape)
-print(field_box)
 
 for row in df.itertuples():
     cv2.rectangle(img1, (row[1],row[2]-height),(row[3],row[4]),(0,0,255),1)
